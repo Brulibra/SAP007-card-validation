169dd46242a8b15f1072845b9aed43b7a1c0eb69
Brulibra <brulibra03@hotmail.com>
2022-01-18 18:02:48 -0300

    alterações 2o dia

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+crediCard()
+crediCardNumber()
+
+let crediCard = document.querySelector('#crediCard').value
+
+
+//document.getElementById("crediCardNumber").innerHTML = (baseElement.querySelector("id=crediCardNumber").innerHTML)
+
+//console.log()
