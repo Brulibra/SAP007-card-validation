58ca6dad82746586e257537a7711997ea200aa5f
Brulibra <brulibra03@hotmail.com>
2022-01-26 17:49:06 -0300

    botão (finalmente) implementado

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import validator from './validator.js';
 
 console.log(validator);
 
+let crediCard = document.getElementById("crediCard") //crediCard é declarado mas nunca lido?
+document.getElementById("button").addEventListener("click",crediCard)
+document.getElementById("button").addEventListener("click",printScreen) //printar resultado na tela
 
-function crediCard() {
-    alert("olá")
-}
+function printScreen(crediCard) {
+    document.write(`<h2>` + `${crediCard}` + `<h2>`) //inner HTML?? ("<h2>" + numbers + "<h2>")
 
-function addEvents() {
-    document.getElementById("crediCard").addEventListener("click",crediCard())
-}
+    }
 
 
-//let numbers = (crediCard)
 
-//console.log(numbers)
-//document.write("<h2>" + numbers + "<h2>")
+
+
+
+
+
+
 
