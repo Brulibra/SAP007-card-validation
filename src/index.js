df5f1ce99c0f6f404b9fb8ba932556c839c7db2b
Brulibra <brulibra03@hotmail.com>
2022-01-27 14:41:22 -0300

    Função crediCardNumber recebendo info

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import validator from './validator.js';
 
 console.log(validator);
 
-let crediCard = document.getElementById("crediCard") //crediCard é declarado mas nunca lido?
-document.getElementById("button").addEventListener("click",crediCard)
-document.getElementById("button").addEventListener("click",printScreen) //printar resultado na tela
+//Elementos HTML
+let crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
+let button = document.getElementById("button"); //variável do botão para guardar os dados recebidos
 
-function printScreen(crediCard) {
-    document.write(`<h2>` + `${crediCard}` + `<h2>`) //inner HTML?? ("<h2>" + numbers + "<h2>")
+//Eventos on Click
+document.getElementById("button").addEventListener("click",crediCardNumber) //envia informações inseridas pelo usiário
+//document.getElementById("button").addEventListener("click",printOnScreen) //printar resultado na tela
 
+function crediCardNumber() {
+    let input = crediCard.value;
+    console.log(input)
+}
+
+/*function printOnScreen(crediCard) {
+    document.write(`<h2>` + `${crediCard}` + `<h2>`) //inner HTML?? ("<h2>" + numbers + "<h2>") 
     }
+    Por resolver ainda
+    */
 
 
 
