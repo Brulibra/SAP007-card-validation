692a721008f88dec80f80941686b891954ea0d40
Brulibra <brulibra03@hotmail.com>
2022-01-31 18:10:04 -0300

    Escopo de variaveis Resolvido, algoritmo de luhn Começo

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import validator from './validator.js';
 
 console.log(validator);
 /*
-//Elementos HTML
-let crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
-let button = document.getElementById("button"); //variável do botão para guardar os dados recebidos, nunca chamada...
+//Elementos do HTML (const escopo global)
+const crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
+const button = document.getElementById("button"); //variável do botão para guardar os dados recebidos, nunca chamada...
 
 //Eventos on Click
-document.getElementById("button").addEventListener("click",crediCardNumber) //envia informações inseridas pelo usiário
-//document.getElementById("button").addEventListener("click",printOnScreen) //printar resultado na tela
+document.getElementById("button").addEventListener("click",isValid) //envia informações inseridas pelo usuário
 
-function crediCardNumber() {
+/*function crediCardNumber() {
     let input = crediCard.value;
     console.log(input)
 }
--- src/validator.js
+++ src/validator.js
@@ -3,20 +3,37 @@ const validator = {
 };
 
 export default validator;
-
-//Elementos do HTML
-let crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
-let button = document.getElementById("button"); //variável do botão para guardar os dados recebidos, nunca chamada...
+//Elementos do HTML (const escopo global)
+const crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
+const button = document.getElementById("button"); //variável do botão para guardar os dados recebidos, nunca chamada...
 
 //Eventos on Click
-document.getElementById("button").addEventListener("click",crediCardNumber) //envia informações inseridas pelo usuário
+document.getElementById("button").addEventListener("click",isValid) //envia informações inseridas pelo usuário
 
-//Recupera e converte de string para número
-function crediCardNumber() {
-  let input = crediCard.value;
-  let stringToNumber = parseInt(input,10);
-  console.log(input, stringToNumber)
+//Recupera dados inseridos pelo usuário (let escopo local)
+function isValid(value) {
+   let valorInput = crediCard.value;
+   let stringToNumber = parseInt(valorInput,10); //e converte de string para número
+    console.log(stringToNumber)
+    
+   //slice -> dividir elementos antes de converter em array, talvez antes de converter em number
+  
+   //transformar valor em array
+  let arrayCardNumber = function(num){
+    return Array.from(num).reverse() //.join('') junta os elementos do array, usar no final
+    }
+   console.log(arrayCardNumber(stringToNumber)); 
+
+  //variável para guardar a os valores em array
+  let arrayCard = arrayCardNumber(stringToNumber);
+  console.log(arrayCard)
+
+  //Laço do Algorítimo de Luhn
+  for (let i=0; i<arrayCard.length; i++) {
+    let indice = arrayCard.indexOf(i !== -1 ); //percorre os indices da array?
+    let array = arrayCard[indice];
+       if (array % 2 == 1){
+          alert("funcionou")
+      }
+   }
 }
-
-
-
