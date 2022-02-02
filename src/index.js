6463373b7ddb93bcc9931e63fc3aec01fb4278c4
Brulibra <brulibra03@hotmail.com>
2022-02-02 18:04:41 -0300

    Algoritmo de Luhn Finalizar, Link Validator&Index Corrigir

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import validator from './validator.js';
 
 console.log(validator);
-/*
+
 //Elementos do HTML (const escopo global)
 const crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
 const button = document.getElementById("button"); //variável do botão para guardar os dados recebidos, nunca chamada...
 
 //Eventos on Click
-document.getElementById("button").addEventListener("click",isValid) //envia informações inseridas pelo usuário
+button.addEventListener("click", isValid()) //envia informações inseridas pelo usuário
 
 /*function crediCardNumber() {
     let input = crediCard.value;
--- src/validator.js
+++ src/validator.js
@@ -1,41 +1,35 @@
+
 const validator = {
-  // ...
-};
-
-export default validator;
-//Elementos do HTML (const escopo global)
-const crediCard = document.getElementById("crediCard"); //crediCard busca a string inserida pelo usuário
-const button = document.getElementById("button"); //variável do botão para guardar os dados recebidos, nunca chamada...
-
-//Eventos on Click
-document.getElementById("button").addEventListener("click",isValid) //envia informações inseridas pelo usuário
-
-//Recupera dados inseridos pelo usuário (let escopo local)
-function isValid() {
-   let valorInput = crediCard.value;
-   console.log(valorInput, typeof valorInput)
-    
-   //transformar valor em array
-   let arrayCardNumber = function(num){
-    return Array.from(num).reverse() //.join('') junta os elementos do array, usar no final
-   }
    
-  //variável para guardar a os valores em array
-  let arrayCard = arrayCardNumber(valorInput);
-  console.log(arrayCard)
+   //Recupera dados inseridos pelo usuário (let escopo local)
+      isValid() {
+      let valorInput = document.getElementById("crediCard")
+      console.log(valorInput, typeof valorInput)
+   
+      //transformar valor em array
+      let arrayCardNumber = function (num) {
+         return Array.from(num).reverse() //.join('') junta os elementos do array, usar no final
+      }
+   
+      //variável para guardar a os valores em array
+      let arrayCard = arrayCardNumber(valorInput);
+      console.log(arrayCard)
+   
+      //Laço do Algoritmo de Luhn 
+      for (let i = 0; i < arrayCard.length; i++) {
 
-  //Laço do Algoritmo de Luhn [CORRIGIR] if esta(va) funcionando com string, corrigir para number
-  for (let i=0; i<arrayCard.length; i++) {
-     
-      let cardNumber = arrayCard.map(Number) //map chama a func. callback dos elementos da array, ignora valores NaN
-      let index = arrayCard[cardNumber]
-      console.log(index, typeof index, typeof cardNumber) //está percorrendo a array
-    
-      if (index % 2 == 1){
-         /*(parseInt(i))*2
-         //alert("funcionou")
-         */
-         console.log(index, typeof index)
+         if (i % 2 == 1) {
+            
+            let numeroDaPosicaoPar = parseInt(arrayCard[i])
+            let numeroMultiplicado = numeroDaPosicaoPar * 2
+            console.log(typeof numeroMultiplicado)
+   
+            if (numeroMultiplicado > 9) {
+               let numeroSomado = numeroMultiplicado - 9
+               console.log(numeroSomado)
+            }
+         }
       }
    }
 }
+export default validator
