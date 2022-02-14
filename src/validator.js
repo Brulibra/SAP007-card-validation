59b53b25cab055f7a993b5855bc7065ba9eed867
Brulibra <brulibra03@hotmail.com>
2022-02-14 13:58:51 -0300

    revisão final, Nomenclaura variáveis

--- src/validator.js
+++ src/validator.js
@@ -2,7 +2,7 @@
 const validator = {
 
    //Recupera dados inseridos pelo usuário (let escopo local)
-   isValid: function (crediCard) {
+   isValid: function (creditCardNumber) {
 
       //transformar valor em array
       let arrayCardNumber = function (num) {
@@ -10,50 +10,49 @@ const validator = {
       }
 
       //variável para guardar a os valores em array
-      let arrayCard = arrayCardNumber(crediCard);
-      console.log(arrayCard)
+      let arrayCard = arrayCardNumber(creditCardNumber);
 
-      let totalValor = 0
+      let totalValue = 0
 
       //Laço do Algoritmo de Luhn 
       for (let i = 0; i < arrayCard.length; i++) {
 
-         let numCardi = parseInt(arrayCard[i]) //array convertido p/ num 
+         let numCard = parseInt(arrayCard[i]) //array convertido p/ num 
 
          if (i % 2 != 0) {
-            let numMultiply = numCardi * 2 // multiplica por 2 nº da posição par
+            let numMultiply = numCard * 2 // multiplica por 2 nº da posição par
 
 
             if (numMultiply > 9) {
                numMultiply = numMultiply - 9
-               totalValor = totalValor + numMultiply
+               totalValue = totalValue + numMultiply
 
                //console.log("entrou no if")
 
             } else {
-               totalValor = totalValor + numMultiply
+               totalValue = totalValue + numMultiply
             }
          } else {
-            totalValor = totalValor + numCardi
+            totalValue = totalValue + numCard
          }
-      } if (totalValor % 10 === 0) {
+      } if (totalValue % 10 === 0) {
          return true
 
       } else {
          return false
       }
    },
-   maskify: function (crediCard) {
-         
-      //guardar string dos primeiros dígitos
-      let maskingNumbers = crediCard.substr(0, crediCard.length)
+   maskify: function (creditCardNumber) {
 
-      for (let i= 0; i < maskingNumbers; i++){
-         let maskOnScreen = maskingNumbers.replace(/\d(?=\d{4})/g, "#") //parâmetros "/\d(?=\d{4})/g" em stack overflow
+      //guardar string numa variável
+      let maskingNumbers = creditCardNumber
+
+      for (let i = 0; i < maskingNumbers; i++) {
+         let maskOnScreen = maskingNumbers.replace(/\d(?=\d{4})/g, "#") //parâmetros "/\d(?=\d{4})/g"
          return maskOnScreen
       }
       return
-     
+
    }
 }
 
@@ -63,4 +62,6 @@ export default validator
 
 --> This reges uses a positive lookahead (?=\d{4}) which means match should be followed by 4 digits.
 --> \d matches a single digit with above lookahead condition and replaces that by #
+
+from stack overflow
 */
